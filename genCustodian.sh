ccf513e017e064aef2b69c9a0ae4cc57df2b0271
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-29 14:43:51 +0000

    Update genCustodian.sh

--- genCustodian.sh
+++ genCustodian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
-echo "$seed_phrase" >> custodian.seed.csv
-echo 0x$(./tonos-cli genpubkey "$seed_phrase" | awk 'FNR == 3 {print $3}') >> custodian.pub.csv
+seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//' | tr -d '"')
+echo "${seed_phrase}" >> custodian.seed.csv
+pubkey=$(./tonos-cli genpubkey "${seed_phrase}" | awk 'FNR == 3 {print $3}')
+printf "0x%s\n" "$pubkey" >> custodian.pub.csv
