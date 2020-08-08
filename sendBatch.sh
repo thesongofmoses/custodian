274b2f1ef214a45be13a33625920997a3b496250
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-08 11:34:33 +0000

    Update sendBatch.sh

--- sendBatch.sh
+++ sendBatch.sh
@@ -16,6 +16,7 @@ do
         ./liteClient.sh getaccount $(sed -n ${i}p "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk '{print $1}') | awk 'FNR == 19 {print $1}' | sed 's/^.\{15\}//' | awk '{print} END {if (!NR) print "empty"}' >> "${1}_status.txt"
 done
 
+rm ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_txid.txt
 for i in {1..100}
 do
 
@@ -26,7 +27,7 @@ cd ${TONOS_CLI_DIR} && \
 ./tonos-cli call "$(cat ${KEYS_DIR}/${2}.addr)" \
         submitTransaction "{"\"dest"\":"\"$(cat "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk "FNR == ${i}" | awk '{print $1}')"\","\"value"\":$(cat "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk "FNR == ${i}" | awk '{print $2}'),"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
         --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-        --sign ${KEYS_DIR}/"${2}.keys.json" | grep 'transId' | awk '{print $2}' | tr -d '"' > "${1}_txid.txt"
+        --sign ${KEYS_DIR}/"${2}.keys.json" | grep 'transId' | awk '{print $2}' | tr -d '"' >> ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_txid.txt
 
 elif [ "$(sed -n ${i}p ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_status.txt)" != 'active' ];
 then
@@ -35,6 +36,6 @@ cd ${TONOS_CLI_DIR} && \
 ./tonos-cli call "$(cat ${KEYS_DIR}/${2}.addr)" \
         submitTransaction "{"\"dest"\":"\"$(cat "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk "FNR == ${i}" | awk '{print $1}')"\","\"value"\":$(cat "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk "FNR == ${i}" | awk '{print $2}'),"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
         --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-        --sign ${KEYS_DIR}/"${2}.keys.json" | grep 'transId' | awk '{print $2}' | tr -d '"' >> "${1}_txid.txt"
+        --sign ${KEYS_DIR}/"${2}.keys.json" | grep 'transId' | awk '{print $2}' | tr -d '"' >> ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_txid.txt
 fi
 done
