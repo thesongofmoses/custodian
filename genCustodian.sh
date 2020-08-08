b24894a18d4872e207defdaa038013effc40076c
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-08 09:42:28 +0000

    Update genCustodian.sh

--- genCustodian.sh
+++ genCustodian.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//' | tr -d '"')
-echo "${seed_phrase}" >> custodian.seed.csv
+if [ -f "${1}_seed.csv" ] || [ -f "${1}_pub.csv" ] || [ -f "${1}_1_keypair.json" ] || [ -f "${1}_2_keypair.json" ] || [ -f "${1}_3_keypair.json" ];
+then
+        echo "Error: duplicate file name(s)"
+        exit 1
+fi
+
+for i in {1..100};
+do
+seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
 pubkey=$(./tonos-cli genpubkey "${seed_phrase}" | awk 'FNR == 3 {print $3}')
-printf "0x%s\n" "$pubkey" >> custodian.pub.csv
+        echo "${seed_phrase}" >> "${1}_seed.csv"
+        printf "0x%s\n" "$pubkey" >> "${1}_pub.csv"
+done
+
+for i in {1..100};
+do
+each_seed_phrase=$(cat "${1}_seed.csv" | awk "FNR == ${i}")
+        ./tonos-cli getkeypair "${1}_${i}_keypair.json" "$each_seed_phrase"
+done
