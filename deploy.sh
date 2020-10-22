733a6366a75270b67d9dd1a97984b8d803ba4a7a
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-22 08:14:58 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/main.ton.dev/tonos-cli/target/release
+
+for i in {8..8};
+do
+pubkey=$(echo 0x$(cat ~/ton-keys/"${1}_${i}.keys.json" | grep 'public' | awk '{print $2}' | tr -d '"'\,))
+signs=(~/ton-keys/"${1}_${i}.keys.json")
+./tonos-cli deploy ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.tvc \
+        "{"\"owners"\":["\"$pubkey"\"],"\"reqConfirms"\":1}" \
+        --abi ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json \
+        --sign "$signs" \
+        --wc 0
+done
