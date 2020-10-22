2a794d8e11b493a370fb39bffbddd7ed0be37cc2
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-22 08:48:07 +0000

    Update contest.sh

--- contest.sh
+++ contest.sh
@@ -14,4 +14,9 @@ each_seed_phrase=$(cat ~/ton-keys/"${1}.seed.csv" | awk "FNR == ${i}")
         ./tonos-cli genaddr ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.tvc \
                 ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json \
                 --setkey ~/ton-keys/"${1}_${i}.keys.json" --wc 0 | awk 'FNR == 9 {print $3}' >> "${1}.raw.csv"
+        ./tonos-cli genaddr ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.tvc \
+                ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json \
+                --setkey ~/ton-keys/"${1}_${i}.keys.json" --wc 0 | awk 'FNR == 9 {print $3}' >> ~/ton-keys/"${1}_${i}.addr"
 done
+
+cat ~/ton-keys/${1}*.addr >> ~/ton-keys/"${1}.addr.csv"
