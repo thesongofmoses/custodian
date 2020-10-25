3d9c7c056d4af91f75da05ce1832219d2edc7b6a
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-25 20:48:08 +0000

    Update and rename contest.sh to contestCreate.sh

--- contestCreate.sh
+++ contestCreate.sh
@@ -3,7 +3,7 @@
 rm ~/ton-keys/${1}.csv
 cd ~/main.ton.dev/tonos-cli/target/release
 
-for i in {1..10};
+for i in {1..X};
 do
 seed_phrase=$(./tonos-cli genphrase | awk 'FNR == 3' | sed 's/^.\{13\}//')
 pubkey=$(./tonos-cli genpubkey "${seed_phrase}" | awk 'FNR == 3 {print $3}')
