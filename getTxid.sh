b9c59558b49aa0f2e881a5bcd1e716e638703778
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-26 05:36:56 +0000

    Update getTxid.sh

--- getTxid.sh
+++ getTxid.sh
@@ -4,6 +4,7 @@ cd ~/main.ton.dev/tonos-cli/target/release
 rm ~/ton-keys/${1}.confirm.txid
 
 for i in {1..x};
+do
 each_raw_addr=$(cat ~/ton-keys/"${1}.raw.csv" | awk "FNR == ${i}")
 ./tonos-cli run $each_raw_addr getTransactions {} \
         --abi ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json >> ~/ton-keys/${1}.confirm.txid
