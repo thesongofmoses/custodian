104c60257dae07606d344268437a7dd9c6c3ca95
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-26 05:51:49 +0000

    Update coldTransaction.sh

--- coldTransaction.sh
+++ coldTransaction.sh
@@ -3,6 +3,7 @@
 cd ~/main.ton.dev/tonos-cli/target/release
 
 for i in {1..x};
+do
 each_raw_addr=$(cat ~/ton-keys/"${1}.raw.csv" | awk "FNR == ${i}")
 each_dest_addr=$(cat ~/ton-keys/"${1}.dest.csv" | awk "FNR == ${i}")
 each_amount=$(cat ~/ton-keys/"${1}.amount.csv" | awk "FNR == ${i}")
