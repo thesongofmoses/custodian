21dae7c8e1e1c713f2ac19bfa8180103d98e0cfa
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-26 04:05:31 +0000

    Update getAmount.sh

--- getAmount.sh
+++ getAmount.sh
@@ -4,6 +4,7 @@ cd ~/main.ton.dev/tonos-cli/target/release
 rm ~/ton-keys/${1}.amount.csv
 
 for i in {1..x};
+each_raw_addr=$(cat ~/ton-keys/"${1}.raw.csv" | awk "FNR == ${i}")
 cd ~/node.operator/scripts && \
         ./liteClient.sh "getaccount ${each_raw_addr}" | tail -n 1 | tr -d '=''ng' | awk '{print $4}') >> ~/ton-keys/${1}.amount.csv
 done
