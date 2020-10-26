accf4a5c4806f88d3f090bd9f0a8840a5353599b
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-10-26 06:26:43 +0000

    Update coldSend.sh

--- coldSend.sh
+++ coldSend.sh
@@ -4,7 +4,7 @@ cd ~/main.ton.dev/tonos-cli/target/release
 
 for i in {1..x};
 do
-each_msg=$(cat ~/ton-keys/${1}.confirm.msg | awk "FNR == ${i}")
+each_msg=$(cat ~/ton-keys/${1}.msg.csv | awk "FNR == ${i}")
 ./tonos-cli send \
         --abi ~/main.ton.dev/ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json \
         "$each_msg"
